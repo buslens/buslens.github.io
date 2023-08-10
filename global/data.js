c8f7e1a028920457a8156ddd8eed353d049cff71
DSS T Level for TMC <107148267+gmgwhDM2005@users.noreply.github.com>
2023-08-10 14:51:25 +0100

    a

---
+++ global/data.js
@@ -0,0 +1,21 @@
+document.getElementById('navigbar').innerHTML=`
+<style>
+    ::-webkit-scrollbar {width: 5px;}
+    ::-webkit-scrollbar-track {background: black;}
+    ::-webkit-scrollbar-thumb {background: rgb(0, 51, 128);}
+    ::-webkit-scrollbar-thumb:hover {background: rgb(0, 53, 96);}
+</style>
+<nav class="navbar">
+    <ul class="navbar-list">
+        <li class="navbar-item">
+            <a href="https://buslens.github.io/LH/buses" class="navbar-link">LH</i></a>
+        </li>
+        <li class="navbar-item">
+            <a href="https://buslens.github.io/DM/buses" class="navbar-link">DM</a>
+        </li>
+        <li class="navbar-item">
+            <a href="https://buslens.github.io" class="navbar-link"><i class="fa fa-home"></i></a>
+        </li>
+    </ul>
+</nav>
+`;
